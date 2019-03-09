5520ab72c09c44a38961fb2fd133c669b8746d71
Mariana Garman <garman.mariana@gmail.com>
2019-03-09 04:53:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,30 +10,30 @@ def loop_iterator(integer = 7)
 end
 end
 
-def times_iterator(integer = 7)
-  7.times do
+def times_iterator(integer = 9)
+  9.times do
     puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
 
-def while_iterator(7)
+def while_iterator(integer = 11)
   counter = 0
-  while counter < 7
+  while counter < 11
   counter += 1
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
 
-def until_iterator(7)
+def until_iterator(integer = 8)
   counter = 0
-  until counter == 7
+  until counter == 8
   puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
 end
 end
 
-def for_iterator(1..7)
-   greeting_count = 1..7
+def for_iterator(1..6)
+   greeting_count = 1..6
   for greeting in greeting_count 
     puts "Welcome to Flatiron School's Web Development Course!"
 end
